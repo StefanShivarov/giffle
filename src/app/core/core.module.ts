3b2c1d7665e17e17e139ece416ccf4bc610ed5a2
StefanShivarov <stefan.shivarov.jr@gmail.com>
2022-06-05 13:42:36 +0300

    Initial commit - basic layout with html and css

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifService } from './service/gif.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ]
+})
+export class CoreModule { 
+
+  static forRoot(): ModuleWithProviders<CoreModule>{
+    return { 
+      ngModule: CoreModule,
+      providers: [
+        GifService,
+      ]
+    }
+  }
+}
---
+++ src/app/search/search.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchPageComponent } from './search-page/search-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    SearchPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    SearchPageComponent
+  ]
+})
+export class SearchModule { }
